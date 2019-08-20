b6e38cfa887cba7778a5da8afaac2781e8d81c68
ゾアン ミン・フック <m.doan@MAC0096.local>
2019-08-20 21:53:23 +0900

    create project

---
+++ src/main/java/bootcamp/co/jp/netprotections/NetprotectionsApplication.java
@@ -0,0 +1,13 @@
+package bootcamp.co.jp.netprotections;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetprotectionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetprotectionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bootcamp/co/jp/netprotections/NetprotectionsApplicationTests.java
@@ -0,0 +1,16 @@
+package bootcamp.co.jp.netprotections;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NetprotectionsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
